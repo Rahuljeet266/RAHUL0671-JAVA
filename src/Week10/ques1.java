903f97d02cb3d2b6789e38676ed1842af60a574a
rahul <rahuljeetsingh0846@gmail.com>
2025-09-15 09:31:53 +0530

    week 10

---
+++ src/Week10/ques1.java
@@ -0,0 +1,39 @@
+package Week10;
+
+public class ques1 {
+    public static void main(String[] args){
+
+                //                            number is even or odd
+//        int n = 5;
+//        if(n%2==0){
+//            System.out.println("Even ");
+//        }
+//        else {
+//            System.out.println("odd");
+//        }
+
+                //               all odd numbers between 1 and 30 using for loop and if statement
+
+//        for(int i = 1;i<=30;i++){
+//            if(i%2!=0){
+//                System.out.print(i+" ");
+//            }
+//        }
+
+
+                // write while loop high for number greater than 5 otherwise low
+
+//                int n = 1;
+//                while(n<11){
+//                    if(n<6){
+//                        System.out.print("LOW ");
+//                    }
+//                    else if(n>5){
+//                        System.out.print("High ");
+//                    }
+//                    n++;
+//                }
+
+            }
+        }
+
---
+++ src/Week10/ques2.java
@@ -0,0 +1,82 @@
+package Week10;
+
+public class ques2 {
+
+    public static void main(String[] args){
+        //  pattern
+
+//        for(int i = 1;i<=5;i++){
+//            for(int j = 1;j<=5-i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1;j<=i;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+//        System.out.println();
+
+        // Square of #
+
+//        for(int i = 1;i<=3;i++){
+//            for(int j = 1;j<=3;j++){
+//                System.out.print("* ");
+//            }
+//            System.out.println();
+//        }
+
+        // square
+
+//        for(int i = 1;i<=5;i++){
+//            for(int j = 1;j<=5;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+        // pattern
+
+//        for(int i = 1;i<=7;i++){
+//            for(int j = 1;j<=7-i+1;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+//        for(int i = 2;i<=7;i++){
+//            for(int j = 1;j<=i;j++){
+//                System.out.print(j+" ");
+//            }
+//            System.out.println();
+//        }
+
+        // diamond
+
+//        Scanner sc = new Scanner(System.in);
+//        int n = sc.nextInt();
+//        for(int i = 1;i<=n;i++){
+//            for(int j = 1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            for(int j = 2;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+//        for(int i = n-1;i>0;i--){
+//            for(int j = 1;j<=n-i;j++){
+//                System.out.print(" ");
+//            }
+//            for(int j = 1;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            for(int j = 2;j<=i;j++){
+//                System.out.print("*");
+//            }
+//            System.out.println();
+//        }
+
+    }
+}
